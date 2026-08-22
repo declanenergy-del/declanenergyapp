a80a0e9f3f39926efae2e47b0e2556c70c85aaa2
declanenergy-del <declanenergy@gmail.com>
2026-08-22 13:34:32 +0100

    uploaded

---
+++ AdsUpload/auth-config.js
@@ -0,0 +1,13 @@
+// Centralized auth config used by both auth.html and the admin auth flow
+export const AUTHORIZED_EMAIL = 'martinsanonye@gmail.com';
+
+export const FIREBASE_CONFIG = {
+ apiKey: "AIzaSyAaROfdTTmJWKMQKVrsUIRwCJWitrqbgqk",
+    authDomain: "declanenergy-29e5d.firebaseapp.com",
+    databaseURL: "https://declanenergy-29e5d-default-rtdb.firebaseio.com",
+    projectId: "declanenergy-29e5d",
+    storageBucket: "declanenergy-29e5d.firebasestorage.app",
+    messagingSenderId: "586531754156",
+    appId: "1:586531754156:web:73070c02c9f48c96880f09",
+    measurementId: "G-DZ69V9TWCX"
+};
